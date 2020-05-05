5206565e6ca7390d5fc2a31f1ea2eea1b9b9e7ab
marinadelafuente <mdelafuente18@gmail.com>
2020-05-05 15:12:08 +0800

    add date, delete and search functions

--- js/app.js
+++ js/app.js
@@ -4,16 +4,21 @@ const search = document.querySelector('.js-search');
 const searchForm = document.querySelector('.js-search-form');
 const taskList = document.querySelector('.js-tasks');
 const addForm = document.querySelector('.js-add');
+const done = document.querySelector('.js-done-tasks');
+const addTask = document.querySelector('.js-add-task');
+const input = document.querySelector('.js-input');
+const taskPlus = document.querySelector('.task-plus');
 
 
 // ADD NEW TASK
 const handlerAddTask = ev => {
     ev.preventDefault()
-    const task = addForm.addTask.value.trim(); // trim remove any white spaces before/after the string
+    const task = input.value.trim(); // trim remove any white spaces before/after the string
     if (task.length) {
         generateTasksHtml(task);
-        addForm.reset(); // resets all the inputs fields inside the form
+        addForm.reset(); // resets all the input fields inside the form
     }
+    drag();
 }
 
 const generateTasksHtml = task => {
@@ -22,16 +27,18 @@ const generateTasksHtml = task => {
     taskList.innerHTML += html;
 }
 
-// DELETE TASKS
-
-const deleteTasks = (ev) => {
-    if (ev.target.classList.contains('delete')) {
-        ev.target.parentElement.parentElement.remove();
+handlerValidation = () => {
+    if (input.value.length) {
+        addTask.classList.add('cursor');
+        taskPlus.classList.add('validated');
     }
-};
+    else {
+        addTask.classList.remove('cursor');
+        taskPlus.classList.remove('validated');
+    }
+}
 
 // SEARCH FOR A TASK
-
 const handlerSearchTask = () => {
     const searchTerm = search.value.trim().toLowerCase();
     filterTasks(searchTerm);
@@ -49,9 +56,18 @@ const filterTasks = (searchTerm) => {
         .forEach(task => task.classList.remove('hidden'))
 }
 
+// DELETE TASKS
+const deleteTasks = (ev) => {
+    if (ev.target.classList.contains('delete')) {
+        ev.target.parentElement.parentElement.remove();
+    }
+};
 
 addForm.addEventListener('submit', handlerAddTask);
+addTask.addEventListener('click', handlerAddTask);
+taskPlus.addEventListener('click', handlerAddTask);
 taskList.addEventListener('click', deleteTasks);
+done.addEventListener('click', deleteTasks);
 search.addEventListener('keyup', handlerSearchTask);
-
+input.addEventListener('keyup', handlerValidation);
 
