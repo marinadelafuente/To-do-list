0721fb29d0619e23c41f840a0e9b9bf2f293e879
marinadelafuente <mdelafuente18@gmail.com>
2020-05-05 11:36:47 +0800

    fix li sizing and new fonts

--- js/app.js
+++ js/app.js
@@ -9,23 +9,49 @@ const addForm = document.querySelector('.js-add');
 // ADD NEW TASK
 const handlerAddTask = ev => {
     ev.preventDefault()
-    console.log(taskList.children);
     const task = addForm.addTask.value.trim(); // trim remove any white spaces before/after the string
     if (task.length) {
         generateTasksHtml(task);
         addForm.reset(); // resets all the inputs fields inside the form
-        console.log(taskList.children);
     }
 }
 
 const generateTasksHtml = task => {
     const now = new Date();
-    const html = `<li class="item-added li list-group-item d-flex justify-content-between align-items-center" draggable="true">${task}</li>`
+    const html = `<li class="item-added li list-group-item d-flex justify-content-between align-items-center">${task}<span class="date">${now.toLocaleDateString()}<i class="fas fa-trash-alt delete"></i></span></li>`
     taskList.innerHTML += html;
+}
+
+// DELETE TASKS
+
+const deleteTasks = (ev) => {
+    if (ev.target.classList.contains('delete')) {
+        ev.target.parentElement.parentElement.remove();
+    }
+};
+
+// SEARCH FOR A TASK
+
+const handlerSearchTask = () => {
+    const searchTerm = search.value.trim().toLowerCase();
+    filterTasks(searchTerm);
+};
+
+const filterTasks = (searchTerm) => {
+    let tasks = Array.from(taskList.children); // we have to transform it into an array to use array methods in it
+
+    tasks
+        .filter(task => !task.textContent.toLowerCase().includes(searchTerm))
+        .forEach(task => task.classList.add('hidden'))
+
+    tasks
+        .filter(task => task.textContent.toLowerCase().includes(searchTerm))
+        .forEach(task => task.classList.remove('hidden'))
 }
 
 
 addForm.addEventListener('submit', handlerAddTask);
-
+taskList.addEventListener('click', deleteTasks);
+search.addEventListener('keyup', handlerSearchTask);
 
 
